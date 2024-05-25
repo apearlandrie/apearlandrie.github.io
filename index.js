aa2351b923ed853a6bc98acd39ec834ea4a902f2
apearlandrie <apearlandrie@gmail.com>
2024-05-25 13:38:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const countLikes2 = document.getElementById("countLikes2")
 function hitLikes1() {
    let totalLikes1 = parseInt(countLikes1.value) + 1
    countLikes1.textContent = totalLikes1.toString()
-}
-function hitLikes2() {
+
+function hitLikes2() 
    let totalLikes2 = parseInt(countLikes2.value) + 1
    countLikes2.textContent = totalLikes2.toString() 
 }
